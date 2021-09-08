260472b02121093d1b10053fde2375a77dd7762c
FieryPotato <65465835+FieryPotato@users.noreply.github.com>
2021-09-08 13:45:35 -0400

    Added BOX_MAP and docstrings to Sudoku methods.

--- src/Sudoku.py
+++ src/Sudoku.py
@@ -5,6 +5,35 @@ from src.Cell import Cell
 
 
 CELL_KEYS: list = [(j, i) for i, j in itertools.product(range(9), repeat=2)]
+BOX_MAP: dict = {
+        1: ((0, 0), (1, 0), (2, 0),
+            (0, 1), (1, 1), (2, 1),
+            (0, 2), (1, 2), (2, 2)),
+        2: ((3, 0), (4, 0), (5, 0),
+            (3, 1), (4, 1), (5, 1),
+            (3, 2), (4, 2), (5, 2)),
+        3: ((6, 0), (7, 0), (8, 0),
+            (6, 1), (7, 1), (8, 1),
+            (6, 2), (7, 2), (8, 2)),
+        4: ((0, 3), (1, 3), (2, 3),
+            (0, 4), (1, 4), (2, 4),
+            (0, 5), (1, 5), (2, 5)),
+        5: ((3, 3), (4, 3), (5, 3),
+            (3, 4), (4, 4), (5, 4),
+            (3, 5), (4, 5), (5, 5)),
+        6: ((6, 3), (7, 3), (8, 3),
+            (6, 4), (7, 4), (8, 4),
+            (6, 5), (7, 5), (8, 5)),
+        7: ((0, 6), (1, 6), (2, 6),
+            (0, 7), (1, 7), (2, 7),
+            (0, 8), (1, 8), (2, 8)),
+        8: ((3, 6), (4, 6), (5, 6),
+            (3, 7), (4, 7), (5, 7),
+            (3, 8), (4, 8), (5, 8)),
+        9: ((6, 6), (7, 6), (8, 6),
+            (6, 7), (7, 7), (8, 7),
+            (6, 8), (7, 8), (8, 8))
+}
 
 
 class Sudoku:
@@ -83,6 +112,8 @@ class Sudoku:
         return self[coordinates].digit
 
     def check_cell_pencil_marks(self, coordinates) -> None:
+        """Clear pencil marks from a cell if the pencil mark appears
+        in the cell's row, column, or box."""
         cell: Cell = self[coordinates]
         row_digits: set = {self[c].digit for c in cell.row}
         column_digits: set = {self[c].digit for c in cell.column}
@@ -92,6 +123,7 @@ class Sudoku:
         
     @classmethod
     def from_string(cls, string) -> "Sudoku":
+        """Return a sudoku whose cells in order appear in an 81-character string."""
         if len(string) != 81:
             raise ValueError
         new = cls()
@@ -99,9 +131,12 @@ class Sudoku:
             new[key].fill(string[i])
         return new
 
-    def update_pencil_marks(self):
+    def update_pencil_marks(self) -> None:
+        """Update all pencil marks in the puzzle based only on cell/row/box
+         logic."""
         for key in self.keys():
             self.check_cell_pencil_marks(key)
 
-    def box(self, i):
-        pass
+    def box(self, i) -> list[Cell]:
+        """Return the list of cells in box i of the Sudoku."""
+        return [self[cell] for cell in BOX_MAP[i]]
