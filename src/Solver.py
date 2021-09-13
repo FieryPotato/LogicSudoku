c70d69b17c050f6b63382f95b52a6377fd8e48c8
FieryPotato <65465835+FieryPotato@users.noreply.github.com>
2021-09-12 21:59:24 -0400

    Solver clears pencil marks in row and column with tests.

--- src/Solver.py
+++ src/Solver.py
@@ -91,11 +91,20 @@ class Solver:
             for box in self.sudoku.boxes:
                 possibles = [cell for cell in box if digit in cell.pencil_marks]
                 rows = set(c.y for c in possibles)
+                columns = set(c.x for c in possibles)
+
                 if len(rows) == 1:
                     pointed_group = self.sudoku.row(list(rows)[0])
                     for cell in pointed_group:
                         if cell not in possibles and digit in cell.pencil_marks:
                             cell.pencil_marks.remove(digit)
+
+                elif len(columns) == 1:
+                    pointed_group = self.sudoku.column(list(columns)[0])
+                    for cell in pointed_group:
+                        if cell not in possibles and digit in cell.pencil_marks:
+                            cell.pencil_marks.remove(digit)
+
 
 
 
--- test/SolverLogic/test_PointingTuple.py
+++ test/SolverLogic/test_PointingTuple.py
@@ -13,12 +13,20 @@ UNSOLVED_ROW: str = "169837254" \
                     "9864 3 25" \
                     "7512    3"
 
-UNSOLVED_COL: str = ""
+UNSOLVED_COL: str = "4 8   713" \
+                    " 37 48295" \
+                    " 253  468" \
+                    " 7 2    6" \
+                    "      3 9" \
+                    "8    4  7" \
+                    "58 4 967 " \
+                    "74 85693 " \
+                    "  6     4"
 
 
 class TestPointingTuple(unittest.TestCase):
     def test_solver_clears_pencil_marks_in_row(self) -> None:
-        sudoku: Sudoku = Sudoku().from_string(UNSOLVED_ROW)
+        sudoku: Sudoku = Sudoku.from_string(UNSOLVED_ROW)
         solver: Solver = Solver(sudoku)
         keys_to_change = ((3, 5), (4, 5))
         pointing_tuple = ((1, 5), (2, 5))
@@ -29,6 +37,20 @@ class TestPointingTuple(unittest.TestCase):
             self.assertFalse(7 in sudoku[key].pencil_marks)
         for key in pointing_tuple:
             self.assertTrue(7 in sudoku[key].pencil_marks)
+
+    def test_solver_clears_pencil_marks_in_col(self) -> None:
+        sudoku: Sudoku = Sudoku.from_string(UNSOLVED_COL)
+        solver: Solver = Solver(sudoku)
+        keys_to_change = ((0, 3), (0, 4), (0, 8))
+        pointing_tuple = ((0, 1), (0, 2))
+
+        solver.check_for_pointing_tuple()
+
+        for key in keys_to_change:
+            self.assertFalse(1 in sudoku[key].pencil_marks)
+        for key in pointing_tuple:
+            self.assertTrue(1 in sudoku[key].pencil_marks)
+
 
 
 if __name__ == '__main__':
