b69bdf124ffe4a68123b48c21a1218ce427acc13
FieryPotato <65465835+FieryPotato@users.noreply.github.com>
2021-11-01 00:47:30 -0400

    formatting

--- test/SolverLogic/test_EasyLogic/test_HiddenSingle.py
+++ test/SolverLogic/test_EasyLogic/test_HiddenSingle.py
@@ -56,5 +56,6 @@ class TestHiddenSingle(unittest.TestCase):
         self.assertTrue(solver.fill_hidden_singles())
         self.assertEqual(1, solver.sudoku[(4, 1)].digit)
 
+
 if __name__ == '__main__':
     unittest.main()
