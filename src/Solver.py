8d068c72319049d52926ff368141ee36aa39c124
FieryPotato <65465835+FieryPotato@users.noreply.github.com>
2021-11-08 01:32:35 -0500

    condensed pointing tuple nested for loop

--- src/Solver.py
+++ src/Solver.py
@@ -114,21 +114,20 @@ class Solver:
 
     def check_for_pointing_tuple(self) -> bool:
         operated = False
-        for digit in range(1, 10):
-            for box in self.sudoku.boxes:
-                possibles = [cell for cell in box if digit in cell.pencil_marks]
-                rows = set(c.y for c in possibles)
-                cols = set(c.x for c in possibles)
-                if len(rows) == 1:
-                    pointed_group = self.sudoku.row(rows.pop())
-                elif len(cols) == 1:
-                    pointed_group = self.sudoku.column(cols.pop())
-                else:
-                    continue
-                for cell in pointed_group:
-                    if cell not in possibles and digit in cell.pencil_marks:
-                        cell.pencil_marks.remove(digit)
-                        operated = True
+        for digit, box in itertools.product(range(1, 10), self.sudoku.boxes):
+            possibles = [cell for cell in box if digit in cell.pencil_marks]
+            rows = set(c.y for c in possibles)
+            cols = set(c.x for c in possibles)
+            if len(rows) == 1:
+                pointed_group = self.sudoku.row(rows.pop())
+            elif len(cols) == 1:
+                pointed_group = self.sudoku.column(cols.pop())
+            else:
+                continue
+            for cell in pointed_group:
+                if cell not in possibles and digit in cell.pencil_marks:
+                    cell.pencil_marks.remove(digit)
+                    operated = True
         return operated
 
     def check_digit_in_cell_for_group_hidden_single(self, digit, cell, group) -> bool:
