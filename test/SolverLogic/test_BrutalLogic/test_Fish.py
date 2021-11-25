3cab221cc7563b8c1d706700486e4c09d0da2358
FieryPotato <65465835+FieryPotato@users.noreply.github.com>
2021-11-25 10:47:49 -0500

    Solver now solves swordfish in rows

--- test/SolverLogic/test_BrutalLogic/test_Fish.py
+++ test/SolverLogic/test_BrutalLogic/test_Fish.py
@@ -14,6 +14,16 @@ COL_SWORDFISH = "7  5  894" \
                 "5  6  218" \
                 "1 29  645"
 
+ROW_SWORDFISH = " 29  4 68" \
+                " 17682 95" \
+                "68 9 3 21" \
+                "9 2 6 8  " \
+                "1  2486 9" \
+                " 68 9 5 2" \
+                "2  7 9186" \
+                "   82 9  " \
+                "89 4  2  "
+
 
 class TestSwordfish(unittest.TestCase):
     def test_solver_clears_swordfish_in_column(self):
@@ -40,6 +50,29 @@ class TestSwordfish(unittest.TestCase):
         for key in cleared_keys:
             self.assertFalse(digit in sudoku[key].pencil_marks)
 
+    def test_solver_clears_swordfish_in_row(self):
+        sudoku = Sudoku.from_string(ROW_SWORDFISH)
+        solver = Solver(sudoku)
+        edited = {
+            (1, 3): {5},
+            (0, 3): {3},
+            (0, 5): {3},
+            (0, 7): {3, 4},
+            (1, 7): {4},
+            (2, 7): {3, 4, 5},
+            (5, 7): {5},
+            (7, 7): {7},
+            (8, 7): {7},
+            (2, 8): {3, 5}
+        }
+        sudoku.post_init(edited)
+        cleared = {(4, 0), (1, 7), (4, 8)}
+        digit = 5
+
+        self.assertTrue(solver.check_for_fish())
+
+        for key in cleared:
+            self.assertFalse(digit in sudoku[key].pencil_marks)
 
 
 if __name__ == '__main__':
